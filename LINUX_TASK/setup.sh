938b4d98d236b21781751202eb4377187b645316
Ayooluwa29 <jesuniyig@gmail.com>
2025-11-20 00:33:30 +0100

    updated the script.sh

--- LINUX_TASK/setup.sh
+++ LINUX_TASK/setup.sh
@@ -5,12 +5,19 @@ SEARCH_VENV=".venv*"
 SEARCH_LOGS="logs*"
 LOG_DIR="$DIR/logs"
 LOG_FILE="${LOG_DIR}/setup.log"
-FIND_VENV=$(find "$DIR" -maxdepth 1 -type d -name "$SEARCH_VENV" -print0 | xargs -0)
-FIND_LOGS=$(find "$DIR" -maxdepth 1 -type d -name "$SEARCH_LOGS" -print0 | xargs -0)
+# FIND_VENV=$(find "$DIR" -maxdepth 1 -type d -name "$SEARCH_VENV" -print0 | xargs -0)
+# FIND_LOGS=$(find "$DIR" -maxdepth 1 -type d -name "$SEARCH_LOGS" -print0 | xargs -0)
+FIND_LOGS=$(find "$DIR" -maxdepth 1 -type d -name "$SEARCH_LOGS" -print -quit)
 IGNORE_FILE="$DIR/.gitignore"
 IGNORE_RULES=("__pycache__/" ".idea/" ".vscode/" ".DS_Store" ".env" "build/" "dist/" "wheels/" ".pytest_cache" "*.log" "*.tmp" "venv/" ".venv/" "env/")
 PACKAGES=("pandas" "numpy" "polars" "pyarrow" "SQLAlchemy" "requests" "beautifulsoup4")
-#mkdir "$LOG_DIR"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
 
 # Create log directory and file if they don't exist
 create_log_dir_file() {
@@ -32,7 +39,7 @@ log_message() {
     local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
     case "$log_type" in
-        INFO)
+        SUCCESS)
             color_code="${GREEN}"
             ;;
         WARNING)
@@ -41,55 +48,44 @@ log_message() {
         ERROR)
             color_code="${RED}"
             ;;
+        INFO)
+            color_code="${CYAN}"
+            ;;
         *)
             color_code="${NC}"
             ;;
     esac
 
-    # print to console with color
-    echo "${color_code}[${timestamp}] [${log_type}] ${message}${NC}" | tee -a "$LOG_FILE"
+    # print to log file and console with color
+    echo -e "${color_code}[${log_type}] [${timestamp}] ${message}${NC}" | tee -a "$LOG_FILE"
 }
-
-
-# Creating a function for logging
-#log() {
-#    local  
-#    if [[ -n "$FIND_LOGS" ]]; then 
-#        echo "$(date) - $1 " | tee -a $LOG_FILE
-#    else
-#       echo "Creating a log directory" | mkdir "$LOG_DIR"
-#       echo "Created log directory: $LOG_DIR"
-#       echo "Creating log file" | touch "$LOG_FILE"
-#       echo "Created log file: $LOG_FILE"
-#       echo "$(date) - $1 " | tee -a $LOG_FILE
-#    fi
-#}
 
 
 # Creating a function for checking if a virtual environment exist
 # If yes, activate the virtual environmet
 # If not create one and activate
 vir_env() {
+    FIND_VENV=$(find "$DIR" -maxdepth 1 -type d -name "$SEARCH_VENV" -print -quit)
     if [[ -n "$FIND_VENV" ]]; then
         log_message INFO "Virtual environment exists: $FIND_VENV"
         if [[ -f "$FIND_VENV/bin/activate" ]]; then
             source "$FIND_VENV/bin/activate"
-            log_message INFO "Virtual environment activated: $FIND_VENV"
+            log_message SUCCESS "Virtual environment activated: $FIND_VENV"
         else
             log_message ERROR "Activate script not found in $FIND_VENV/bin/"
             return 1
         fi
     else
-        log_message "No virtual environment found in "$DIR""
+        log_message INFO "No virtual environment found in "$DIR""
         echo "Enter a name for your virtual environment:"
         read venv_name
-        log_message "Creating virtual environment: $venv_name"
+        log_message INFO "Creating virtual environment: $venv_name"
         python3 -m venv "$venv_name"
-        if [[ -f "$venv_name/bin/activate" ]]; then
-            log_message INFO "Created virtual environment: $venv_name"
-            source "$venv_name/bin/activate"
+        if [[ -f "$DIR/$venv_name/bin/activate" ]]; then
+            log_message SUCCESS "Created virtual environment: $venv_name"
+            source "$DIR/$venv_name/bin/activate"
         else
-            log_message ERROR "Activate script not found in $venv_name/bin/"
+            log_message ERROR "Activate script not found in $DIR/$venv_name/bin/"
             return 1
         fi
     fi
@@ -100,19 +96,19 @@ upgrade_pip() {
     # Check if pip is installed
     # Installs the latest pip version
     if  ! command -v pip &> /dev/null ; then
-        log_message "pip not installed. Installing latest pip..."
+        log_message INFO "pip not installed. Installing latest pip..."
         python3 -m ensurepip --upgrade --default-pip
         if [ $? -eq 0 ]; then
-            log_message INFO "latest pip installed successfully."
+            log_message SUCCESS "latest pip installed successfully."
         else
             log_message ERROR "Failed to install/upgrade pip."
             return 1
         fi
     else
-        log_message "pip is already installed. Upgrading to the latest..."
+        log_message INFO "pip is already installed. Upgrading to the latest..."
         python3 -m pip install --upgrade pip
         if [ $? -eq 0 ]; then
-            log_message INFO "pip upgraded successfully."
+            log_message SUCCESS "pip upgraded successfully."
         else
             log_message ERROR "Failed to upgrade pip."
             return 1
@@ -124,29 +120,32 @@ upgrade_pip() {
 # Create gitignore file function
 gitignore() {
     if [[ ! -f "$IGNORE_FILE" ]]; then
-        log_message "Creating $IGNORE_FILE and adding igore rules..."
+        log_message INFO "Creating $IGNORE_FILE and adding igore rules..."
+        # for element in "${IGNORE_RULES[@]}"; do
         for element in "${IGNORE_RULES[@]}"; do
-            log_message "$element" >> "$IGNORE_FILE"
+            echo "$element" >> "$IGNORE_FILE"
         done
+        log_message SUCCESS "$IGNORE_FILE successfully created with ignore rules"
     else
-        log_message "$IGNORE_FILE already exists."
+        log_message INFO "$IGNORE_FILE already exists."
     fi
 }
 
 
 # Install python packages
 install_packages() {
-    log_message "Installing Python packages..."
+    log_message INFO "Installing Python packages..."
+    # for package in "${PACKAGES[@]}"; do
     for package in "${PACKAGES[@]}"; do
-        log_message "Installing $package..."
+        log_message INFO "Installing $package..."
         pip install "$package"
         if [ $? -ne 0 ]; then
-            log_message WARNING " Error installing $package. Exiting"
+            log_message ERROR " Error installing $package. Exiting"
             exit 1
         fi
     done
     
-    echo "All packages installed successfully."
+    log_message SUCCESS "All packages installed successfully."
 }
 
 
@@ -159,7 +158,9 @@ main() {
     upgrade_pip
     gitignore
     install_packages
-    log_mesage INFO "Main execution finished."
+    log_message SUCCESS "Main execution finished."
+    log_message SUCCESS "WORK ENVIRONMENT READY! - SETUP COMPLETE!"
+    log_message SUCCESS "HAVE FUN BUILDING!"
 }
 
 # call main function
